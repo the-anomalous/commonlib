1e85805f331f19db476c83b01f2637c757c7f562
anomalous <adityamusale4477@gmail.com>
2024-09-29 19:14:11 +0530

    configure tsc-alias

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-// if (process.env.NODE_ENV !== 'production') {
-  // require('tsconfig-paths/register')
-// }
-
-
 // Errors 
 export * from "./errors/bad-request.error";
 export * from "./errors/custom-abs.error";
